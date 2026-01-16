d5ad6ccc227e1e59f5baf63943d8779a8fd52d57
Mambesi <demmambesi@gmail.com>
2026-01-16 15:24:00 +0200

    Initial commit

---
+++ BlazorApp1/Models/DeviceType.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1.Models
+{
+    public enum DeviceType
+    {
+        BarcodeScanner,
+        Printer,
+        Camera,
+        SocketTray
+    }
+}
---
+++ BlazorApp1/Models/Operation.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Operation
+    {
+        public int OperationID { get; set; }
+        public string Name { get; set; }
+        public int OrderInWhichToPerform { get; set; }
+        public byte[] ImageData { get; set; }
+        public Device Device { get; set; }
+    }
+}
